8ff23a863767a38dff1d5f725b6e947a122e3e3f
Florian Gudat <florian.gudat@guices.com>
2020-01-05 21:56:43 +0100

    Update

--- frontend/src/api/fragments/webcomponent.ts
+++ frontend/src/api/fragments/webcomponent.ts
@@ -1,51 +1,93 @@
 import {
-  EventDrivenConsumer,
-  EventDrivenConsumerMS,
-  IEventDrivenConsumer,
-  Logger,
-  Message
+    EventDrivenConsumerMS,
+    IEventDrivenConsumer,
+    Logger,
+    Message
 } from "@scsa/messaging";
 import "../../client/index.css";
 import { cfg } from "../../config";
-import tpl from "../../server/views/partials/entry.pug";
+import controls from "../../server/views/partials/controls.pug";
+import entry from "../../server/views/partials/entry.pug";
 
 const eventDrivenConsumer = new EventDrivenConsumerMS(cfg);
 
 class CheckoutBasket extends HTMLElement implements IEventDrivenConsumer {
-  private logger: Logger;
+    private logger: Logger;
 
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    this.render();
+        this.render();
 
-    const button = this.shadowRoot.querySelector("button");
-    button.addEventListener("click", this.handleClick);
+        const button = this.shadowRoot.querySelector("button");
+        button.addEventListener("click", this.handleClick);
 
-    this.logger = new Logger({ctx: this.shadowRoot});
+        this.logger = new Logger({ ctx: this.shadowRoot });
 
-    eventDrivenConsumer.subscribe(this);
-  }
+        eventDrivenConsumer.subscribe(this);
+    }
 
-  public render() {
-    const template = document.createElement("template");
-    template.innerHTML += tpl();
-    template.innerHTML += `<link type="text/css" rel="stylesheet" href="${cfg
-      .CURRENT.options.url + "api/fragments/webcomponent.css"}">`;
-    this.attachShadow({ mode: "open" });
-    this.shadowRoot.appendChild(template.content.cloneNode(true));
-  }
+    public render() {
+        const template = document.createElement("template");
+        template.innerHTML += entry();
+        template.innerHTML += `<link type="text/css" rel="stylesheet" href="${cfg
+            .CURRENT.options.url + "api/fragments/webcomponent.css"}">`;
+        this.attachShadow({ mode: "open" });
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+    }
 
-  public handleClick(event) {
-    eventDrivenConsumer.publish(
-      new Message({ purchase: { products: ["Product 1"] } })
-    );
-  }
+    public handleClick(event) {
+        eventDrivenConsumer.publish(
+            new Message({ purchase: { products: ["Product 1"] } })
+        );
+    }
 
-  public callback(data) {
-    this.logger.write(data);
-  }
+    public callback(data) {
+        this.logger.write(data);
+    }
 }
 
+// tslint:disable-next-line:max-classes-per-file
+class CheckoutControls extends HTMLElement implements IEventDrivenConsumer {
+    private logger: Logger;
 
+    constructor() {
+        super();
+
+        this.render();
+
+        const button = this.shadowRoot.querySelector("button");
+        button.addEventListener("click", this.handleClick);
+
+        this.logger = new Logger({ ctx: this.shadowRoot });
+
+        eventDrivenConsumer.subscribe(this);
+    }
+
+    get value() {
+        return
+    }
+
+    public render() {
+        const template = document.createElement("template");
+        const styling = this.getAttribute('styling');
+        template.innerHTML += controls({styling});
+        template.innerHTML += `<link type="text/css" rel="stylesheet" href="${cfg
+            .CURRENT.options.url + "api/fragments/webcomponent.css"}">`;
+        this.attachShadow({ mode: "open" });
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+    }
+
+    public handleClick(event) {
+        eventDrivenConsumer.publish(
+            new Message({ purchase: { add: ["Product 1"] } })
+        );
+    }
+
+    public callback(data) {
+        this.logger.write(data);
+    }
+}
+
+globalThis.customElements.define("checkout-controls", CheckoutControls);
 globalThis.customElements.define("checkout-basket", CheckoutBasket);
